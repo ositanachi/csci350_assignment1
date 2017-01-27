f48e080e56ce80059335a97134b09234bda03823
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-01-27 00:37:18 -0500

    Problem 1: Initial commit...programming initial logic

---
+++ fp.rkt
@@ -0,0 +1,15 @@
+;Reverse a list
+;Mindset: Make use of car, cdr, and append
+;Car to take the first cell, cdr for the rest, and append to combine
+
+> (define (reverse-general L)
+   (if (null? L) '() ;If list is null...
+    (append (reverse-general (cdr L)
+                             (car L)
+                            )
+            )
+    )
+   )
+    
+
+> (reverse-general '(5 a b (4 5) 3))

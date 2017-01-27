2f71d5e4ebd8677d340e5394573eeb978f636538
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-01-27 13:20:30 -0500

    Problem 2: Function implentation
    Trying to get function add num

--- fp.rkt
+++ fp.rkt
@@ -10,20 +10,20 @@
 
 > (reverse-general '(1 (2 3) (4 (a (b (c d))))))
 
-;2
-;While numlist is not null and if the atom is a num add
-;> (define (sum-up-numbers-simple L)
-;    (if (null? L) 0) ;Checks if list is empty, if so 0
-;        (if (number? (car L)) ;Checking if element is a num
-;            (+ (car L) ;Add element
-;               (sum-up-numbers-simple (cdr L))))) ;Perform procedure again until list is empty
-
+;Implementation of number list
 > (define (is-num L)
     (cond ((null? L) '())
           ((number? (car L)) (cons (append (car L))
                              (is-num(cdr L))))
           (else (is-num (cdr L)))))
+;2
+;While numlist is not null and if the atom is a num add
+> (define (sum-up-numbers-simple L)
+    (cond ((null? L) 0) ;Checks if list is empty, if so 0
+          (list? L) (+ (sum-up-numbers-simple L (car L)) (sum-up-numbers-simple (cdr L))
+              (+ (car L) (sum-up-numbers-simple (cdr L))))
+          ;(sum-up-numbers-simple L(((is-num(L)))))
+          (else 0)))
 
-    
-;> (sum-up-numbers-simple '(4 5 6 7))
-> (is-num '(a 7 5 w 5 6 5))
+
+sum-up-numbers-simple '((a 100 b (200) c 300 d))

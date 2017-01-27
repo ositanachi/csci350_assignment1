bac38a7b192eb24631e3860d1a5c906e52da419a
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-01-27 13:42:34 -0500

    Problem 4
    starting implementation for problem 4

--- fp.rkt
+++ fp.rkt
@@ -20,11 +20,13 @@
 ;While numlist is not null and if the atom is a num add
 > (define (sum-up-numbers-simple L)
     (cond ((null? L) 0) ;Checks if list is empty, if so 0
-          ((not ((list? L)) 0)) ;Checks if it is in fact a list
-          ((not (number? (car L))) (sum (cdr L)))
-          (else (+ ((sum-up-numbers-simple (car L)) ( (sum-up-numbers-simple L (car L)) (sum-up-numbers-simple (cdr L))
-              (+ (car L) (sum-up-numbers-simple (cdr L)))))))))
-          ;(sum-up-numbers-simple L(((is-num(L)))))
+          ((not (list? L)) 0) ;Checks if it is in fact a list
+          ((not (number? (car L))) (sum (cdr L))) ;If it's not a number continue with the rest
+          (else (+ (sum-up-numbers-simple (car L)) (sum-up-numbers-simple L (cdr L)))
+              (+ (car L) (sum-up-numbers-simple (cdr L))))))
 
+;(sum-up-numbers-simple '(300 400 500))
 
-sum-up-numbers-simple '(300 400 500)
+;4
+(define (min-above-min L1 L2)
+  (cond ((nul? L1) #f) ((null? l2)
